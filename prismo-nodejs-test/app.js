66dbc2a25a84e053e8e98c23adda971918afc1a7
CamilleChlc <camille.chaulaic@gmail.com>
2019-06-03 10:11:53 +0200

    Partie 2 question 3

--- prismo-nodejs-test/app.js
+++ prismo-nodejs-test/app.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(ctx => {
+router.get('/welcome', (ctx,next) => {
 	ctx.body = 'Hello World';
-});
+})
+
+app.use(router.routes()).use(router.allowedMethods());
+
+
 
 app.listen(3000);//
