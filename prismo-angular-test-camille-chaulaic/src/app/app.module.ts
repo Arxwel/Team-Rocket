65df0f3c3eb33656a03437d688cfcf162467b130
CamilleChlc <camille.chaulaic@gmail.com>
2019-06-03 21:09:58 +0200

    partie 1 question 2

--- prismo-angular-test-camille-chaulaic/src/app/app.module.ts
+++ prismo-angular-test-camille-chaulaic/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes =[
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '', redirectTo: 'welcome', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule,
     AppRoutingModule
   ],
